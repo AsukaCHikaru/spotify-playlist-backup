93db63381576d9d088840e1b19be76644a6bb964
Asuka Wang <hungmao.wang@linecorp.com>
2024-02-21 22:41:22 +0900

    feat: fetch tracks from playlist

--- main.go
+++ main.go
@@ -28,6 +28,28 @@ type Playlists struct {
 	Items []PlaylistItem `json:"items"`
 }
 
+type PlaylistItemList struct {
+	Items []TrackItem `json:"items"`
+}
+
+type TrackItem struct {
+	Track Track `json:"track"`
+}
+
+type Track struct {
+	Name    string   `json:"name"`
+	Artists []Artist `json:"artists"`
+}
+
+type Playlist2 struct {
+	Name  string
+	Songs []Track
+}
+
+type Artist struct {
+	Name string `json:"name"`
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -42,5 +64,22 @@ func main() {
 	if err != nil {
 		fmt.Println(err.Error())
 		return
+	}
+
+	for i := range list.Items {
+		playlistApiEndpoint := list.Items[i].Url
+		playlistItemsResponse := apiCore.Fetch(playlistApiEndpoint+"/tracks", client, authResponse.AccessToken)
+		var list2 PlaylistItemList
+		err = json.Unmarshal([]byte(playlistItemsResponse), &list2)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		var result Playlist2
+		result.Name = list.Items[i].Name
+		for i := range list2.Items {
+			result.Songs = append(result.Songs, list2.Items[i].Track)
+		}
+		fmt.Println((result))
 	}
 }
